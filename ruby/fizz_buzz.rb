9381bcc8ecc5b0c559ac4ea74e31ebca27ee6d26
EC2 Default User <ec2-user@ip-172-31-42-4.ap-northeast-1.compute.internal>
2023-04-16 13:49:40 +0000

    [Finish] 2-8

---
+++ ruby/fizz_buzz.rb
@@ -0,0 +1,17 @@
+def fizz_buzz(number)
+  if (number % 3 == 0) && (number % 5 != 0)
+    return "Fizz" 
+  elsif (number % 3 != 0) && (number % 5 == 0)
+    return "Buzz"
+  elsif (number % 3 == 0) && (number % 5 == 0)
+    return "FizzBuzz"
+  else
+    return number
+  end
+end
+
+puts "数字を入力してください"
+number = gets.to_i
+# puts "#{number}"
+puts "結果は..."
+puts fizz_buzz(number)
---
+++ ruby/greeting.rb
@@ -0,0 +1,6 @@
+def greeting(name)
+  return "Hello,#{name}!"
+  "Good morning, #{name}!"
+end
+
+puts greeting("John")
