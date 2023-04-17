0e1579e83d23a0b41b5e0fab6b4ebce82102a964
EC2 Default User <ec2-user@ip-172-31-42-4.ap-northeast-1.compute.internal>
2023-04-17 10:30:20 +0000

    [Fisnish] 2-11

---
+++ ruby/override.rb
@@ -0,0 +1,19 @@
+class Car
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+class Bus < Car
+  def run(distance)
+    super
+    puts "30人を乗せて、走っています。"
+    # puts "バスで#{distance}キロ走ります。"
+  end
+end
+
+bus = Bus.new
+bus.run(5)
+
+# car = Car.new
+# car.run(5)
